12f8f191eda812c248593cd6fa4ab58fb70388a6
Vinay288 <vinayhiremath288@gmail.com>
2021-11-10 12:58:42 +0530

    ADDED:on change event binding

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component{
   constructor(){
     super()
     this.state={
-      userName:"Vinay"
+      userName:"",
+      nameError:""
     }
   }
   onClick=($event) =>{
@@ -15,11 +16,26 @@ class App extends React.Component{
     window.open(this.url);
   }
 
+  onNameChange=(event)=>{
+    console.log("value is ",event.target.value);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    this.setState({userName:event.target.value});
+    if(nameRegex.test(event.target.value)){
+      this.setState({nameError:""})
+    }
+    else{
+      this.setState({nameError:"Invalid Name"})
+    }
+  }
   render(){
     return(
       <div>
+      <div>
       <h1>Hello {this.state.userName}</h1>
         <img src={logo} onClick={this.onClick} alt="loaded"/>
+        </div>
+        <input onChange={this.onNameChange}/>
+        <span className="error-output">{this.state.nameError}</span>
         </div>
     );
 }
