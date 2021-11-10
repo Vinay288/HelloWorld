39096226af60d71fd9a81eec8ad9005940d6dead
Vinay288 <vinayhiremath288@gmail.com>
2021-11-11 00:09:50 +0530

    ADDED: routing for home page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Route,Routes,BrowserRouter } from 'react-router-dom'  
+import Home from './Home';
 
 const routing = (  
   <BrowserRouter>  
-  <Routes>
-      <Route path="/home" element={<App />} />    
-      <Route path={"/" | "/abc"} element={<App />} />  
-    </Routes>
+      <Routes>
+    
+      <Route path="/" element={<App />} ></Route> 
+      <Route path="home" element={<Home />} ></Route> 
+
+      </Routes>
+      
+    
   </BrowserRouter>  
 )  
 ReactDOM.render(
