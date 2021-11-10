b1cfac925dfcbcfd519fb07236ac78be724269cd
Vinay288 <vinayhiremath288@gmail.com>
2021-11-10 12:39:03 +0530

    ADDED: onclick event

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import React from 'react';
 import logo from "../src/assests/image/bridgelabz.png"
 
 class App extends React.Component{
+  url="https://www.bridgelabz.com/"
   constructor(){
     super()
     this.state={
       title:"hello world"
     }
   }
+  onClick=($event) =>{
+    console.log("button is clicked!",$event);
+    window.open(this.url);
+  }
 
   render(){
     return(
       <div>
       <h1>{this.state.title}</h1>
-        <img src={logo} alt="loaded"/>
+        <img src={logo} onClick={this.onClick} alt="loaded"/>
         </div>
     );
 }
