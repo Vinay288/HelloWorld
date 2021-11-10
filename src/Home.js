cedeca2d5419c93dacd80db45fd94812ea39b615
Vinay288 <vinayhiremath288@gmail.com>
2021-11-11 00:08:48 +0530

    ADDED:html code for home using flex

---
+++ src/Home.js
@@ -0,0 +1,75 @@
+import "./Home.css"
+import React from "react";
+class Home extends React.Component{
+render() {
+return (
+<>
+<div>
+<nav class="col-1">
+        <ul>
+            <li><a href="html_css_flexbox_image_gallery.html">My Photos</a></li>
+            <li><a href="html_css_flexbox_cards_stretch.html">Details</a></li>
+        </ul>
+    </nav>
+    <div class="col-2">
+        <header>Welcome to bridgelabz</header>
+        <main class="content">
+            <article>
+                <h2 >Bridge Labz</h2>
+                <div>
+                    <h3>About</h3>
+                    Bridgelabz is an AWS recognised incubator and is soley focused on solving tech employability. Over
+                    the
+                    last 5 years, it has worked with 500+ top tech companies and 2500+ engineers have been hired from
+                    Bridgelabz. It has labs in Mumbai and Bangalore. It has partnered with over 500 companies, including
+                    Yatra, Capgemini, Meru Cabs, LafargeHolcim, JDA, UrbanLadder, Fullerton India, and RBL bank.
+                    Bridgelabz
+                    recently won Business World Techtor Award 2020 and secured 2nd position at Global Edtech Startup
+                    Awards
+                    2019, India. Also, was selected by the Maharashtra state government as Top 100 Startups 2019. More
+                    than
+                    350+ learners placed in last 6 months despite covid downturn while most organizations were not
+                    hiring or
+                    were laying off!
+                </div>
+                <br/>
+                BridgeLabz is an AWS recognised incubator and is solely focussed on solving tech employability. Over the
+                last 4 years, it has worked with 500+ top tech companies and 2500+ engineers have been hired from
+                Bridgelabz. It has labs in Mumbai and Bangalore. It has partnered with over 500+ companies, including
+                Yatra, Capgemini, Meru Cabs, LafargeHolcim, JDA, UrbanLadder, Fullerton India, and RBL bank. BridgeLabz
+                won Business World Techtor Award 2020 and secured 2nd position at Global Edtech Startup Awards 2019,
+                India. Also, was selected by the Maharashtra state government as Top 100 Startups 2019. More than 250+
+                learners placed in the last 6 months despite covid downturn while most organizations were not hiring or
+                were laying off!
+                <br/>
+                BridgeLabz is an AWS recognised incubator and is solely focussed on solving tech employability. Over the
+                last 4 years, it has worked with 500+ top tech companies and 2500+ engineers have been hired from
+                Bridgelabz. It has labs in Mumbai and Bangalore. It has partnered with over 500+ companies, including
+                Yatra, Capgemini, Meru Cabs, LafargeHolcim, JDA, UrbanLadder, Fullerton India, and RBL bank. BridgeLabz
+                won Business World Techtor Award 2020 and secured 2nd position at Global Edtech Startup Awards 2019,
+                India. Also, was selected by the Maharashtra state government as Top 100 Startups 2019. More than 250+
+                learners placed in the last 6 months despite covid downturn while most organizations were not hiring or
+                were laying off!
+                <br/>
+                BridgeLabz is an AWS recognised incubator and is solely focussed on solving tech employability. Over the
+                last 4 years, it has worked with 500+ top tech companies and 2500+ engineers have been hired from
+                Bridgelabz. It has labs in Mumbai and Bangalore. It has partnered with over 500+ companies, including
+                Yatra, Capgemini, Meru Cabs, LafargeHolcim, JDA, UrbanLadder, Fullerton India, and RBL bank. BridgeLabz
+                won Business World Techtor Award 2020 and secured 2nd position at Global Edtech Startup Awards 2019,
+                India. Also, was selected by the Maharashtra state government as Top 100 Startups 2019. More than 250+
+                learners placed in the last 6 months despite covid downturn while most organizations were not hiring or
+                were laying off!
+            </article>
+            </main>
+        <footer>Commercial</footer>
+        <p id="footerline">
+           
+        </p>
+
+    </div>
+    </div>
+    </>
+);
+}
+}
+export default Home;
